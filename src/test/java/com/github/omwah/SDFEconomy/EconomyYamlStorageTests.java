12065d11338c73eef3486db982754ca52d424b4e
Omwah <omwah@towhee.org>
2012-11-26 23:04:09 -0800

    Add stub for starting work on unit tests

---
+++ src/test/java/com/github/omwah/SDFEconomy/EconomyYamlStorageTests.java
@@ -0,0 +1,20 @@
+/*
+ */
+package com.github.omwah;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ *
+ */
+@RunWith(JUnit4.class)
+public class EconomyYamlStorageTests {
+    @Test
+    public void testWrite() {
+        
+    }
+}
