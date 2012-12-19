cd234c0430d6cf7a2f5caf012d1e39601289f1f9
Omwah <omwah@oyeh.org>
2012-12-19 14:23:43 -0800

    Actually include BankInfoCommand in commit

---
+++ src/main/java/com/github/omwah/SDFEconomy/commands/BankInfoCommand.java
@@ -0,0 +1,60 @@
+package com.github.omwah.SDFEconomy.commands;
+
+import com.github.omwah.SDFEconomy.BankAccount;
+import java.util.List;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import com.google.common.base.Joiner;
+
+import com.github.omwah.SDFEconomy.SDFEconomyAPI;
+import net.milkbowl.vault.economy.EconomyResponse;
+import net.milkbowl.vault.economy.EconomyResponse.ResponseType;
+
+public class BankInfoCommand extends BasicCommand {
+
+    private SDFEconomyAPI api;
+
+    public BankInfoCommand(SDFEconomyAPI api) {
+        super("bank info");
+
+        this.api = api;
+        
+        setDescription("Displays bank account balance and members");
+        setUsage(this.getName() + " §8<account_name>");
+        setArgumentRange(1, 1);
+        setIdentifiers(this.getName());
+        setPermission("sdfeconomy.use_bank");
+    }
+
+    @Override
+    public boolean execute(CommandHandler handler, CommandSender sender, String label, String identifier, String[] args)
+    {
+        String account_name = args[0];
+        BankAccount account = api.getBankAccount(account_name);
+
+        if (account != null) {
+            if(handler.hasAdminPermission(sender) || 
+                    sender instanceof Player && account.isOwner(((Player)sender).getName()) ||
+                                                account.isMember(((Player)sender).getName())) {
+                sender.sendMessage("§c-----[ " + "§f Bank Info: " + account_name + " §c ]-----");
+                sender.sendMessage("Location: " + account.getLocation());
+                sender.sendMessage("Balance: " + api.format(account.getBalance()));
+                sender.sendMessage("Owner: " + account.getOwner());
+
+                Joiner joiner = Joiner.on(", ");
+                String member_list = joiner.join(account.getMembers());
+                sender.sendMessage("Members: " + member_list);
+            } else {
+                sender.sendMessage("You are not the owner or a member of the bank: " + account_name);
+                return false;
+            }
+        } else {
+            sender.sendMessage("No bank named " + account_name + " was found");
+            return false;
+        }
+            
+        return true;
+    }
+   
+}
